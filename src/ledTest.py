88e63efd372b8cd272715ee8d0f621254f9ddb9d
Achterberg <manuel.achterberg@Mac.fritz.box>
2024-12-16 10:05:04 +0100

    changed to common anode rgb led

--- src/ledTest.py
+++ src/ledTest.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 # Pin Definitions
-RED_PIN = 22  # Replace these if necessary
-GREEN_PIN = 23
+RED_PIN = 23
+GREEN_PIN = 22
 BLUE_PIN = 24
 
 # Setup
@@ -13,24 +13,36 @@ GPIO.setup(GREEN_PIN, GPIO.OUT)
 GPIO.setup(BLUE_PIN, GPIO.OUT)
 
 try:
-    while True:
-        print("Testing Red")
-        GPIO.output(RED_PIN, GPIO.LOW)  # LOW for common anode, HIGH for common cathode
-        sleep(2)
-        GPIO.output(RED_PIN, GPIO.HIGH)
-        
-        print("Testing Green")
-        GPIO.output(GREEN_PIN, GPIO.LOW)
-        sleep(2)
-        GPIO.output(GREEN_PIN, GPIO.HIGH)
-        
-        print("Testing Blue")
-        GPIO.output(BLUE_PIN, GPIO.LOW)
-        sleep(2)
-        GPIO.output(BLUE_PIN, GPIO.HIGH)
+    print("Testing RED")
+    GPIO.output(RED_PIN, GPIO.LOW)  # LOW = ON for common anode
+    sleep(1)
+    GPIO.output(RED_PIN, GPIO.HIGH)  # HIGH = OFF
+    sleep(1)
+
+    print("Testing GREEN")
+    GPIO.output(GREEN_PIN, GPIO.LOW)
+    sleep(1)
+    GPIO.output(GREEN_PIN, GPIO.HIGH)
+    sleep(1)
+
+    print("Testing BLUE")
+    GPIO.output(BLUE_PIN, GPIO.LOW)
+    sleep(1)
+    GPIO.output(BLUE_PIN, GPIO.HIGH)
+    sleep(1)
+
+    print("WHITE (All Colors ON)")
+    GPIO.output(RED_PIN, GPIO.LOW)
+    GPIO.output(GREEN_PIN, GPIO.LOW)
+    GPIO.output(BLUE_PIN, GPIO.LOW)
+    sleep(2)
+
+    print("OFF")
+    GPIO.output(RED_PIN, GPIO.HIGH)
+    GPIO.output(GREEN_PIN, GPIO.HIGH)
+    GPIO.output(BLUE_PIN, GPIO.HIGH)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
-    print("Exiting...")
 finally:
     GPIO.cleanup()
