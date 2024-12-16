470ee8ade09fa9cd21c463bd4ed2c2499104842c
Achterberg <manuel.achterberg@Mac.fritz.box>
2024-12-16 10:10:21 +0100

    led testing again

--- src/ledTest.py
+++ src/ledTest.py
@@ -14,43 +14,35 @@ GPIO.setup(BLUE_PIN, GPIO.OUT)
 
 try:
     while True:
-        # Test Red
-        print("Red ON")
-        GPIO.output(RED_PIN, GPIO.LOW)  # LOW = ON for common anode
-        sleep(2)
-        print("Red OFF")
-        GPIO.output(RED_PIN, GPIO.HIGH)  # HIGH = OFF
-        sleep(1)
-
-        # Test Green
-        print("Green ON")
-        GPIO.output(GREEN_PIN, GPIO.LOW)
-        sleep(2)
-        print("Green OFF")
+        # Test Red Only
+        print("Testing Red")
+        GPIO.output(RED_PIN, GPIO.LOW)  # LOW for common anode, HIGH for common cathode
         GPIO.output(GREEN_PIN, GPIO.HIGH)
-        sleep(1)
-
-        # Test Blue
-        print("Blue ON")
-        GPIO.output(BLUE_PIN, GPIO.LOW)
-        sleep(2)
-        print("Blue OFF")
         GPIO.output(BLUE_PIN, GPIO.HIGH)
-        sleep(1)
+        sleep(2)
 
-        # Test White (All Colors ON)
-        print("White ON")
-        GPIO.output(RED_PIN, GPIO.LOW)
+        # Test Green Only
+        print("Testing Green")
+        GPIO.output(RED_PIN, GPIO.HIGH)
         GPIO.output(GREEN_PIN, GPIO.LOW)
+        GPIO.output(BLUE_PIN, GPIO.HIGH)
+        sleep(2)
+
+        # Test Blue Only
+        print("Testing Blue")
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        GPIO.output(GREEN_PIN, GPIO.HIGH)
         GPIO.output(BLUE_PIN, GPIO.LOW)
         sleep(2)
-        print("White OFF")
+
+        # Turn Everything OFF
+        print("All Off")
         GPIO.output(RED_PIN, GPIO.HIGH)
         GPIO.output(GREEN_PIN, GPIO.HIGH)
         GPIO.output(BLUE_PIN, GPIO.HIGH)
-        sleep(1)
+        sleep(2)
 
 except KeyboardInterrupt:
-    print("Exiting...")
+    GPIO.cleanup()
 finally:
     GPIO.cleanup()
