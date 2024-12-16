60383825a2a72c46523e1bd43331b1efb6b14444
Achterberg <manuel.achterberg@Mac.fritz.box>
2024-12-16 11:14:13 +0100

    led script update

--- src/led.py
+++ src/led.py
@@ -1,36 +1,27 @@
 from gpiozero import RGBLED
 from time import sleep
 
-# Pin Definitions (adjust as per your setup)
-led = RGBLED(red=22, green=23, blue=8, active_high=False)  # active_high=False for common cathode
+# Pin Definitions (adjust if needed)
+led = RGBLED(red=22, green=23, blue=8, active_high=True)  # Default active_high=True for common cathode
 
 try:
     while True:
-        # Red ON
-        print("Red ON")
+        print("Red")
         led.color = (1, 0, 0)  # Red
         sleep(2)
 
-        # Green ON
-        print("Green ON")
+        print("Green")
         led.color = (0, 1, 0)  # Green
         sleep(2)
 
-        # Blue ON
-        print("Blue ON")
+        print("Blue")
         led.color = (0, 0, 1)  # Blue
         sleep(2)
 
-        # White (All Colors ON)
-        print("White ON")
-        led.color = (1, 1, 1)  # White (all ON)
-        sleep(2)
-
-        # All OFF
-        print("All OFF")
+        print("Off")
         led.off()
         sleep(2)
 
 except KeyboardInterrupt:
     print("Exiting...")
-    led.off()  # Turn off the LED on exit
+    led.off()
